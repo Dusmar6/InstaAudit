9b202112e26dfc81b889d7ad577b6148c9a290ff
kevvvinreed <33916889+kevvvinreed@users.noreply.github.com>
2020-04-14 21:29:47 -0700

    Update Cars.py

--- ML Code/Cars.py
+++ ML Code/Cars.py
@@ -3,35 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
-from sklearn import preprocessing 
-import seaborn as sns
 
 cars = pd.read_csv('cars.csv')
 
 print(cars)
-
-# Create Plot
-fig = cars[cars.maker=='America'].plot(kind='scatter', x='horsepower', y='weight', color='purple', label='America')
-cars[cars.maker=='Europe'].plot(kind='scatter', x='horsepower', y='weight', color='red', label='Europe', ax=fig)
-cars[cars.maker=='Asia'].plot(kind='scatter', x='horsepower', y='weight', color='green', label='Asia', ax=fig)
-fig.set_xlabel("Car Horsepower")
-fig.set_ylabel("Car Weight")
-fig.set_title("Car Horsepower vs Weight")
-fig = plt.gcf()
-fig.set_size_inches(10,6)
-plt.show()
-
-cars.maker.unique()
-
-le = preprocessing.LabelEncoder() 
-selected_col = ['maker'] 
-cars[selected_col] = cars[selected_col].apply(le.fit_transform)
-
-plt.figure(figsize=(7,4))  
-sns.heatmap(cars.corr(), annot=True, cmap='PiYG') 
-plt.show()
-
-print(cars.describe())
 
 X = cars[['modelyear', 'acceleration', 'weight', 'horsepower', 'displacement', 'cylinders']]
 
@@ -45,6 +20,14 @@ clsDT.fit(X_train, y_train)
 
 y_pred = clsDT.predict(X_test)
 
-print("\nAccuracy is ", accuracy_score(y_test, y_pred) * 100)
+print("Accuracy is ", accuracy_score(y_test, y_pred) * 100)
 
-
+fig = cars[cars.mpg==1].plot(kind='scatter', x='horsepower', y='weight', color='purple', label='1')
+cars[cars.mpg==2].plot(kind='scatter', x='horsepower', y='weight', color='red', label='2', ax=fig)
+cars[cars.mpg==3].plot(kind='scatter', x='horsepower', y='weight', color='green', label='3', ax=fig)
+fig.set_xlabel("Car Horsepower")
+fig.set_ylabel("Car Weight")
+fig.set_title("Car Horsepower vs Weight")
+fig = plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
