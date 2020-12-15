573493577fa004683d34f865f82b3a02dfedd54e
Tylar Simone <tylarsimone@hotmail.com>
2020-12-14 19:37:36 -0800

    All pages direct accordingly

--- MERN Login System/src/App.js
+++ MERN Login System/src/App.js
@@ -24,18 +24,18 @@ const useStyles = makeStyles((theme) => ({
     overflowX: 'hidden'
   },
 
-	// #global-container {
-	//   opacity: 1;
-	//   transition: 500ms;
-	//   transform: translateX(0);
-	//   transform-origin: left;
+  // #global-container {
+  //   opacity: 1;
+  //   transition: 500ms;
+  //   transform: translateX(0);
+  //   transform-origin: left;
   //   }
 
-	// html.is-animating #global-container {
-	// 	opacity: 1;
-	// 	transform: translateX(100%);
-	// }
-	
+  // html.is-animating #global-container {
+  // 	opacity: 1;
+  // 	transform: translateX(100%);
+  // }
+
 
   /* Loading animation */
   // load: {
@@ -59,19 +59,19 @@ const useStyles = makeStyles((theme) => ({
   //   margin: '0',
   //   animation: 'transform(rotate(90)) 3s' 
   // }
-  
+
   /* Scrollbar */
   '@global': {
     '*::-webkit-scrollbar-track': {
       border: '1px solid black',
       backgroundColor: '#F5F5F5'
     },
-    
+
     '*::-webkit-scrollbar': {
       width: '8px',
       backgroundColor: '#F5F5F5'
     },
-    
+
     '*::-webkit-scrollbar-thumb': {
       backgroundColor: '#000000'
     }
@@ -79,7 +79,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const App = (props) => {
-  
+
   const classes = useStyles();
 
   const initialSession = {
@@ -87,49 +87,58 @@ export const App = (props) => {
   };
 
   const [session, setSession] = useState(initialSession);
-  
+
   useMemo(() => {
     if (localStorage.getItem('token') === null) {
-      setSession({ jwt: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxMDkyMzcwOTEyMX0.Z6NTR6AhWXJlC9y5GI9pBc_fm3Wc6n7sZYlrdXEOKvY' }) 
+      setSession({ jwt: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxMDkyMzcwOTEyMX0.Z6NTR6AhWXJlC9y5GI9pBc_fm3Wc6n7sZYlrdXEOKvY' })
     }
     else {
-      setSession({ jwt: localStorage.getItem('token') }) 
+      setSession({ jwt: localStorage.getItem('token') })
     }
-  }, []) 
+  }, [])
 
   return (
     <div className="App" class={classes.App}>
       <BrowserRouter>
         <Suspense fallback={<div class={classes.load}></div>}>
           <AnimatePresence>
-                <Switch>
-                  <Route path='/' exact strict>
-                    <LandingPage></LandingPage>
-                  </Route>
-                  <Route path='/how-it-works' exact strict>
-                    <HowItWorks></HowItWorks>
-                  </Route>
-                  <Route path='/technologies-used' exact strict>
-                    <TechnologiesUsed></TechnologiesUsed>
-                  </Route>
-                  <Route path='/forgot-password' exact strict>
-                    <ForgotPassword></ForgotPassword>
-                  </Route>
-                  <Route path='/users/sign-in' exact strict>
-                    <SignIn appProps={{session}} setSession={setSession}></SignIn>
-                  </Route>
-                  <Route path='/users/sign-up' exact strict>
-                    <SignUp></SignUp>
-                  </Route>
-                  <ProtectedRoute
-                    path='/api/dashboard'
-                    exact strict
-                    setSession={setSession}
-                    appProps={{session}}
-                    component={Dash}>
-                  </ProtectedRoute>
-                </Switch>
-            </AnimatePresence>
+            <Switch>
+              <Route path='/' exact strict>
+                <LandingPage></LandingPage>
+              </Route>
+              <Route path='/support' exact strict>
+                <LandingPage></LandingPage>
+              </Route>
+              <Route path='/how-it-works' exact strict>
+                <HowItWorks></HowItWorks>
+              </Route>
+              <Route path='/about' exact strict>
+                <HowItWorks></HowItWorks>
+              </Route>
+              <Route path='/technologies-used' exact strict>
+                <TechnologiesUsed></TechnologiesUsed>
+              </Route>
+              <Route path='/discover' exact strict>
+                <TechnologiesUsed></TechnologiesUsed>
+              </Route>
+              <Route path='/forgot-password' exact strict>
+                <ForgotPassword></ForgotPassword>
+              </Route>
+              <Route path='/users/sign-in' exact strict>
+                <SignIn appProps={{ session }} setSession={setSession}></SignIn>
+              </Route>
+              <Route path='/users/sign-up' exact strict>
+                <SignUp></SignUp>
+              </Route>
+              <ProtectedRoute
+                path='/api/dashboard'
+                exact strict
+                setSession={setSession}
+                appProps={{ session }}
+                component={Dash}>
+              </ProtectedRoute>
+            </Switch>
+          </AnimatePresence>
         </Suspense>
       </BrowserRouter>
     </div>
